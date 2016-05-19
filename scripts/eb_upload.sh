b7c3e484580fdeb7863c59ca75ef86698045a4b1
Srinivas Sunkari <srinivas.sunkari@laterooms.com>
2016-05-19 13:58:44 +0100

    Modified the upload script

--- scripts/eb_upload.sh
+++ scripts/eb_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 EB_APP_NAME=$1
-eb init "$EB_APP_NAME" -r eu-west-1
-eb config put rates-quer-int
-eb config put rates-quer-qa
-eb config put rates-quer-qa-perf
-eb config put rates-quer-uat
+`eb init "$EB_APP_NAME" -r eu-west-1`
+`eb config put rates-quer-int`
+`eb config put rates-quer-qa`
+`eb config put rates-quer-qa-perf`
+`eb config put rates-quer-uat`
