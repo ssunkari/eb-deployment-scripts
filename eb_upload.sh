446bbfa8ee84fc5c712b34013aa245164f0b6c2d
Srinivas Sunkari <srinivas.sunkari@laterooms.com>
2016-05-19 17:57:31 +0100

    Modified the upload script to verify the upload artefacts

--- eb_upload.sh
+++ eb_upload.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 EB_APP_NAME=$1
-eb init "$EB_APP_NAME" -r eu-west-1
-eb config put rates-query-int
-eb config put rates-query-qa
-eb config put rates-query-qa-perf
-eb config put rates-query-uat
+EB_ENVIRONMENTS=$2
+eb init "$EB_APP_NAME" -r eu-west-1 --quiet
+echo 'finished'
+ENVS="$(echo $EB_ENVIRONMENTS | sed "s/,/ /g")"
+
+for env in $ENVS
+do
+  echo "Uploading Saved Config for ENV $env"
+  eb config put "$env"
+done
+
+getSavedConfigs=`eb config list`
+echo "list of configs fetched from S3 are \n $getSavedConfigs"
+
+for env in $ENVS
+do
+	configExists=`eb config list | grep ^"$env"$`
+	if [ -z "$configExists" ]; then
+		echo "Upload Failed, $env saved config is missing in S3"
+		exit 1
+	fi
+done
+
+echo 'Upload finished successfully'
