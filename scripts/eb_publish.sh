8e3632bf274313db62415ff5f52b43dfbdf46f74
Barry Dobson <barry.dobson@laterooms.com>
2016-05-19 14:26:24 +0100

    Move to subfolder

--- scripts/eb_publish.sh
+++ scripts/eb_publish.sh
@@ -14,11 +14,11 @@ ZIP=$VERSION.zip
 
 existing_app=`aws elasticbeanstalk describe-application-versions --application-name "$EB_APP_NAME" --version-label "$VERSION" --query "ApplicationVersions[*].VersionLabel" --output text`
 if [ "$existing_app" != "$VERSION" ]; then
-	sudo sed -i='' "s/<AWS_ACCOUNT_ID>/$AWS_ACCOUNT_ID/" Dockerrun.aws.json
+	sudo sed -i "s/<AWS_ACCOUNT_ID>/$AWS_ACCOUNT_ID/" Dockerrun.aws.json
 	sudo sed -i "s/<NAME>/$EB_APP_NAME/" Dockerrun.aws.json
 	sudo sed -i "s/<PORT>/$PORT/" Dockerrun.aws.json
-	sudo sed -i='' "s/<TAG>/$SHA1/" Dockerrun.aws.json
-	sudo sed -i='' "s/<NODE_ENV>/$NODE_ENV/" .ebextensions/env-vars.config
+	sudo sed -i "s/<TAG>/$SHA1/" Dockerrun.aws.json
+	sudo sed -i "s/<NODE_ENV>/$NODE_ENV/" .ebextensions/env-vars.config
 
 	sudo zip -r $ZIP Dockerrun.aws.json .ebextensions
 
